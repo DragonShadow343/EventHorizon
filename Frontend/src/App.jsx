bc060f29947e09232a8d1c41c96cc5a2502c802b
DragonShadow343 <krish_khullar@icloud.com>
2026-02-19 14:55:19 -0800

    Added react-router-dom package for routing and created links to the pages accessible by unregistered users only right now

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,22 @@
 import './App.css'
 import Home from './pages/Home'
+import Events from './pages/Events';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
 
   return (
-    <>
-      <Home/>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/events' element={<Events />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- Frontend/src/components/HomeNavBar.jsx
+++ Frontend/src/components/HomeNavBar.jsx
@@ -1,14 +1,30 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 
 const HomeNavBar = ({className}) => {
   return (
     <>
         <nav className='flex shadow-xl h-20 w-3/5 mx-auto my-15 px-10 items-center justify-between rounded-full'>
-            <h1 className='text-2xl'>EventHorizon</h1>
+
+            <Link to="/">
+              <h1 className='text-2xl'>EventHorizon</h1>
+            </Link>
             <ul className='flex gap-x-5'>
-                <li className='cursor-pointer'>Events</li>
-                <li className='cursor-pointer'>Sign Up</li>
-                <li className='cursor-pointer'>Login</li>
+                <li className='cursor-pointer'>
+                  <Link to="/events">
+                    Events
+                  </Link>
+                </li>
+                <li className='cursor-pointer'>
+                  <Link to="/signup">
+                    Sign Up
+                  </Link>
+                </li>
+                <li className='cursor-pointer'>
+                  <Link to="/login">
+                    Login
+                  </Link>
+                </li>
             </ul>
         </nav>
     </>
