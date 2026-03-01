8c260a04112f4542d86cf1578a61cdcdf66b0ec8
sk114343 <159204395+sk114343@users.noreply.github.com>
2026-03-01 15:35:32 -0800

    Added basic text boxes and layout

--- Frontend/src/pages/Signup.jsx
+++ Frontend/src/pages/Signup.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
 import Navbar from '../components/NavBar/Navbar'
 
 const Signup = () => {
   return (
     <>
       <Navbar />
+      <head>
+        <title className='text-2xl border-l-2'>Sign Up</title>
+      </head>
+      <body className='bg-white scroll-smooth'>
+        <div className='float-left w-1/2'>
+          <h1 className='text-4xl text-center p-4 font-bold'>Sign Up</h1>
+          <input type="text" placeholder="Username" className='p-2 w-full'/>
+          <input type="email" placeholder="Email" className='p-2 w-full'/>
+          <input type="password" placeholder="Password" className='p-2 w-full'/>
+          <button className='bg-blue-950 rounded text-white p-2 w-1/2'>Sign Up Here</button>
+        </div>
+        <figure>
+          <img src="" alt="Image place holder" className='float-right w-1/2'/>
+        </figure>
+      </body>
     </>
   )
 }
