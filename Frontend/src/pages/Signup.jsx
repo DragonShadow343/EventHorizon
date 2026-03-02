0c247487fa1cfe1123d9c230117aec51b495e630
sk114343 <159204395+sk114343@users.noreply.github.com>
2026-03-02 02:17:12 -0800

    Added CSS styling to sign-up page and linked it to log-in page

--- Frontend/src/pages/Signup.jsx
+++ Frontend/src/pages/Signup.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router'
 import Navbar from '../components/NavBar/Navbar'
 
 const Signup = () => {
@@ -7,13 +8,14 @@ const Signup = () => {
       <head>
         <title className='text-2xl border-l-2'>Sign Up</title>
       </head>
-      <body className='bg-white scroll-smooth'>
+      <body className='bg-gray-100 scroll-smooth'>
         <div className='float-left w-1/2'>
-          <h1 className='text-4xl text-center p-4 font-bold'>Sign Up</h1>
-          <input type="text" placeholder="Username" className='p-2 w-full'/>
-          <input type="email" placeholder="Email" className='p-2 w-full'/>
-          <input type="password" placeholder="Password" className='p-2 w-full'/>
-          <button className='bg-blue-950 rounded text-white p-2 w-1/2'>Sign Up Here</button>
+          <h1 className='text-5xl text-center p-4 font-bold mt-35 mb-15'>Sign Up</h1>
+          <input type="text" placeholder="Username" className='p-2 w-60 block mx-auto  border-gray border-1 rounded-2xl my-5 bg-gray-200'/>
+          <input type="email" placeholder="Email" className='p-2 w-60 block mx-auto border-gray border-1 rounded-2xl my-5 bg-gray-200'/>
+          <input type="password" placeholder="Password" className='p-2 w-60 block mx-auto border-gray border-1 rounded-2xl my-5 bg-gray-200'/>
+          <button className='bg-blue-950 rounded-2xl text-white font-semibold p-2 w-32 block mx-auto mt-8'>Sign Up</button>
+          <button className='bg-sky-700 rounded-2xl text-white font-semibold p-2 w-32 block mx-auto mt-2'><NavLink to="/login">Log In</NavLink></button>
         </div>
         <figure>
           <img src="" alt="Image place holder" className='float-right w-1/2'/>
