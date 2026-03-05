5cec18518ea2ff0080a7303ef0ad7c980ce4af9a
sk114343 <159204395+sk114343@users.noreply.github.com>
2026-03-04 17:13:29 -0800

    Added nav bar

--- Frontend/src/pages/events/EventDetails.jsx
+++ Frontend/src/pages/events/EventDetails.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import Navbar from '../../components/NavBar/Navbar'
 
 const EventPage = () => {
   return (
-    <div>EventDetailsPage</div>
+    <>
+      <Navbar />
+    </>
   )
 }
 
