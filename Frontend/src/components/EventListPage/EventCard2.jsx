492cd2834a3dbf3d54694d8d5152917b23282450
sk114343 <159204395+sk114343@users.noreply.github.com>
2026-03-13 16:29:27 -0700

    Added Basic UI details for events list page

---
+++ Frontend/src/components/EventListPage/EventCard2.jsx
@@ -0,0 +1,30 @@
+const EventCard2 = ({ event }) => {
+    return (
+        <div className="flex bg-white rounded-lg shadow p-4 w-full h-40 ml-1 mr-6 my-6">
+            {/* image placeholder */}
+            <div className="relative w-1/4">
+                <div className="bg-gray-200 h-full" />
+                <div className="absolute inset-0 flex items-center justify-start">
+                </div>
+            </div>
+
+            {/* content */}
+            <div className="flex-1 p-6 flex flex-col justify-between">
+                <div className="text-lg font-bold">
+                    {event && event.name ? event.name : "Event Name"}
+                </div>
+
+                <div className="text-gray-600">
+                    {event && event.desc ? event.desc : "Event Description"}
+                </div>
+
+                <div className="flex items-center text-blue-500">
+                    <span className="font-medium">More info</span>
+                    <span className="ml-2">→</span>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default EventCard2
