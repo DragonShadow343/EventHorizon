9aa142af32c56a9e1067426d438558ff84b92a7b
sk114343 <159204395+sk114343@users.noreply.github.com>
2026-03-16 22:26:45 -0700

    Started details page

--- Frontend/src/pages/events/EventDetails.jsx
+++ Frontend/src/pages/events/EventDetails.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import Navbar from '../../components/NavBar/Navbar'
 
 const EventPage = () => {
   return (
-    <div>EventDetailsPage</div>
+    <div>
+      <Navbar />
+
+      {/*Image Placeholder*/}
+      <div className = 'bg-gray-300 h-96 flex items-center justify-center m-10 rounded-lg'>
+        <span className = 'text-2xl text-gray-700'>Event Image</span>
+      </div>
+
+      {/*Event Details*/}
+      <h1 className = 'text-3xl font-bold text-center'>Event Title</h1>
+    </div>
   )
 }
 
