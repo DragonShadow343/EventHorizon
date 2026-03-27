ea5f89e150b09db72b2acdeb08593ea2f7b806cd
sk114343 <159204395+sk114343@users.noreply.github.com>
2026-03-27 15:44:54 -0700

    Added basic UI, and placeholders

--- Frontend/src/pages/admin/AdminDashboard.jsx
+++ Frontend/src/pages/admin/AdminDashboard.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import AdminNavBar from "../../components/NavBar/AdminNavbar"
 
 const AdminDashboard = () => {
   return (
-    <div>AdminDashboard</div>
+    <div>
+      <AdminNavBar />
+      <h1 className="text-3xl font-bold mt-10 ml-6 mb-5">Welcome to the Admin Dashboard</h1>
+      <div className = "flex-col">
+        <div className = "flex">
+          <div className="w-2/3 h-60 bg-amber-200 m-4 rounded-2xl shadow"></div>
+          <div className="w-1/3 h-60 bg-amber-200 m-4 rounded-2xl shadow"></div>
+        </div>
+        <div className = "flex">
+          <div className="w-full h-90 bg-amber-200 m-4 rounded-2xl shadow"></div>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
