38a15eef371979a82bd5c03ff5bc3c4c46779140
sk114343 <159204395+sk114343@users.noreply.github.com>
2026-03-27 15:55:12 -0700

    Changed layout of UI

--- Frontend/src/pages/admin/AdminDashboard.jsx
+++ Frontend/src/pages/admin/AdminDashboard.jsx
@@ -6,12 +6,15 @@ const AdminDashboard = () => {
       <AdminNavBar />
       <h1 className="text-3xl font-bold mt-10 ml-6 mb-5">Welcome to the Admin Dashboard</h1>
       <div className = "flex-col">
-        <div className = "flex">
-          <div className="w-2/3 h-60 bg-amber-200 m-4 rounded-2xl shadow"></div>
-          <div className="w-1/3 h-60 bg-amber-200 m-4 rounded-2xl shadow"></div>
+        <div className = "flex ml-1">
+          <div className="w-full h-40 bg-amber-200 mx-3 my-3 rounded-2xl shadow"></div>
         </div>
         <div className = "flex">
-          <div className="w-full h-90 bg-amber-200 m-4 rounded-2xl shadow"></div>
+          <div className="w-3/5 h-75 bg-amber-200 m-3 rounded-2xl shadow"></div>
+          <div className="w-2/5 h-75 bg-amber-200 m-3 rounded-2xl shadow"></div>
+        </div>
+        <div className = "flex">
+          <div className="w-full h-90 bg-amber-200 m-3 rounded-2xl shadow"></div>
         </div>
       </div>
 
