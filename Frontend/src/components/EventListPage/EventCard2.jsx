7e3cc19f5a311fa0cad8200f920a691d2500e5bd
DragonShadow343 <krish_khullar@icloud.com>
2026-04-10 17:59:31 -0700

    Fixed some UI bugs in the events page

--- Frontend/src/components/EventListPage/EventCard2.jsx
+++ Frontend/src/components/EventListPage/EventCard2.jsx
@@ -2,17 +2,17 @@ const EventCard2 = ({ event, onClick }) => {
     return (
         <div
           onClick={onClick}
-          className="flex cursor-pointer flex-col gap-4 rounded-lg border border-gray-100 bg-white p-4 shadow sm:flex-row sm:items-stretch"
+          className="flex cursor-pointer flex-col gap-4 rounded-lg border border-gray-100 bg-gray-100 p-4 shadow sm:flex-row sm:items-stretch"
         >
             {/* image placeholder */}
-            <div className="relative w-full sm:w-56">
-                <img src={event.imageURL} alt={event.name} className="h-36 w-full rounded object-cover sm:h-full" />
+            <div className="relative rounded-lg overflow-clip w-full bg-white max-h-40 sm:w-80">
+                <img src={event.imageURL} alt={event.name} className="h-36 sm:min-h-40 w-full object-cover sm:h-full" />
                 <div className="absolute inset-0 flex items-center justify-start">
                 </div>
             </div>
 
             {/* content */}
-            <div className="flex flex-1 flex-col justify-between gap-3 sm:p-2">
+            <div className="flex flex-1 flex-col justify-start gap-3 sm:p-2">
                 <div className="text-lg font-bold sm:text-xl">
                     {event && event.name ? event.name : "Event Name"}
                 </div>
@@ -20,11 +20,11 @@ const EventCard2 = ({ event, onClick }) => {
                 <div className="line-clamp-3 text-gray-600">
                     {event && event.desc ? event.desc : "Event Description"}
                 </div>
+            </div>
 
-                <div className="flex items-center text-blue-500">
-                    <span className="font-medium">More info</span>
-                    <span className="ml-2">→</span>
-                </div>
+            <div className="flex items-end text-blue-500">
+                <span className="font-medium">More info</span>
+                <span className="ml-2">→</span>
             </div>
         </div>
     );
